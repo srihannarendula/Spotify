a9a722625f07bcac1762c2590f43aef7970f8650
srihannarendula <srihannarendula@gmail.com>
2026-03-22 15:23:05 +0530

    Initial commit - Youtify Flutter app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -12,111 +15,297 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
+      body: SafeArea(child: ListView(
+        padding: EdgeInsetsGeometry.all(15),
+        children: [
+          const Text(
+            "Good Morning"
+            , style: TextStyle(fontSize: 26,fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 10,),
+          // GridView.count(
+          //   crossAxisCount: 2,
+          //   childAspectRatio: 3.5,
+          //   shrinkWrap: true,
+          //   crossAxisSpacing: 10,
+          //   mainAxisSpacing: 10,
+          //   physics: NeverScrollableScrollPhysics(),
+          //   children: List.generate(
+          //       8,
+          //           (index) =>
+          //           Container(
+          //             decoration: BoxDecoration(
+          //                 color: Colors.grey[850],
+          //                 borderRadius: BorderRadius.circular(10)
+          //             ),
+          //             child: Row(
+          //               children: [
+          //                 Container(
+          //                   width: 50,
+          //                   decoration: BoxDecoration(
+          //                       color: Colors.grey[800],
+          //                       borderRadius: BorderRadius.horizontal(left: Radius.circular(10))
+          //                   ),
+          //                 ),
+          //                 SizedBox(width: 10,)
+          //                 ,
+          //                 Text("Playlist Title")
+          //               ],
+          //             ),
+          //           )
           //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
+          //   ),
           //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          // ),
+          SizedBox(
+            height: 250,
+            child: FutureBuilder(
+                future: fetchSongs(),
+
+                builder:(context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const CircularProgressIndicator();
+                  }
+                  final songs = snapshot.data as List<dynamic>;
+
+                  return  GridView.builder(
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: 3.5,
+
+                      crossAxisSpacing: 10,
+                      mainAxisSpacing: 10,
+                    ),
+                    itemBuilder: (BuildContext context, int index) {
+                      final song = songs[index];
+                      return Flexible(
+                        child: Container(
+                          width: 10,
+                          decoration: BoxDecoration(
+                              color: Colors.grey[850],
+                              borderRadius: BorderRadius.circular(10)
+                          ),
+                          child: Row(
+                            children: [
+                              Container(
+                                  width: 50,
+                                  decoration: BoxDecoration(
+                                      color: Colors.grey[800],
+                                      borderRadius: BorderRadius.horizontal(left: Radius.circular(10))
+
+                                  ),
+                                  child: Image.network(
+                                    song["artworkUrl100"],
+                                    height: 150,
+                                    width: 150,
+                                    fit: BoxFit.cover,
+                                  )
+                              ),
+                              SizedBox(width: 5,)
+                              ,
+                              Text(song["collectionName"].substring(0,15),style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold),
+                                overflow: TextOverflow.fade,)
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  );
+
+                }
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+
+          ),
+
+          SizedBox(height: 10,),
+          Text("Jump back in", style: TextStyle(fontSize: 26 , fontWeight: FontWeight.bold),),
+          SizedBox(
+            height: 250,
+            child: FutureBuilder(
+                future: fetchSongs(),
+
+                builder:(context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const CircularProgressIndicator();
+                  }
+                  final songs = snapshot.data as List<dynamic>;
+
+                  return ListView.builder(
+                      shrinkWrap: false,
+                      itemCount: songs.length,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, index) {
+                        final song = songs[index];
+                        return Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: SizedBox(
+                              width: 150,
+                              height: 100,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Container(
+                                      color: Colors.grey[800],
+                                      height: 150,
+                                      child: Image.network(
+                                        song["artworkUrl100"],
+                                        height: 150,
+                                        width: 150,
+                                        fit: BoxFit.cover,
+                                      )
+                                  ),
+                                  Text(
+                                    song["trackName"], style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w400),
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                  Text(song["collectionName"], style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w400),
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                  Text(song["artistName"], style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w400),
+                                    overflow: TextOverflow.ellipsis,
+                                  )
+                                ],
+                              ),
+
+                            )
+                        );
+                      }
+
+                  );
+                }
+            ),
+
+          ),
+          SizedBox(height: 10,),
+          Text("Popular Albums and singles", style: TextStyle(fontSize: 26 , fontWeight: FontWeight.bold),),
+          SizedBox(
+            height: 250,
+            child: FutureBuilder(
+                future: fetchSongs(),
+
+                builder:(context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const CircularProgressIndicator();
+                  }
+                  final songs = snapshot.data as List<dynamic>;
+
+                  return ListView.builder(
+                      shrinkWrap: false,
+                      itemCount: songs.length,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, index) {
+                        final song = songs[index];
+                        return Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: SizedBox(
+                              width: 150,
+                              height: 100,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Container(
+                                      color: Colors.grey[800],
+                                      height: 150,
+                                      child: Image.network(
+                                        song["artworkUrl100"],
+                                        height: 150,
+                                        width: 150,
+                                        fit: BoxFit.cover,
+                                      )
+                                  ),
+                                  Text(
+                                    song["trackName"], style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w400),
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                  Text(song["collectionName"], style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w400),
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                  Text(song["artistName"], style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w400),
+                                    overflow: TextOverflow.ellipsis,
+                                  )
+                                ],
+                              ),
+
+                            )
+                        );
+                      }
+
+                  );
+                }
+            ),
+
+          )
+          // child: ListView(
+          //     shrinkWrap: false,
+          //     scrollDirection: Axis.horizontal,
+          //
+          //     children: List.generate(10, (index) =>
+
+          //         ),)
+          // ),
+
+        ],
+      )),
+      bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: Colors.grey,
+          unselectedItemColor: Colors.white,
+          showUnselectedLabels: true,
+          selectedLabelStyle: TextStyle(color: Colors.grey),
+          unselectedLabelStyle: TextStyle(color: Colors.white),
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home) , label: "Home"),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
+            BottomNavigationBarItem(icon: Icon(Icons.book) ,  label: "Playlist"),
+            BottomNavigationBarItem(icon: Icon(Icons.person) ,  label: "Settings")
+
+          ]
       ),
     );
+  }
+}
+
+Future<List<dynamic>> fetchSongs() async {
+  final response = await http.get(
+    Uri.parse('https://itunes.apple.com/search?term=arjit&entity=song&limit=20'),
+  );
+  if (response.statusCode == 200) {
+    final data = jsonDecode(response.body);
+    return data["results"];
+  } else {
+    throw Exception("Failed to load songs");
   }
 }
